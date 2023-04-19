ac5c3c0633d2016557f05e86ed8f4a1a6fb04f64
jerryliang122 <jerryliang@jerryliang.top>
2023-04-19 12:39:03 +0000

    当30分钟没有访问回收模型

--- views/origin.py
+++ views/origin.py
@@ -6,23 +6,26 @@ import datetime
 import threading
 
 origin_bp = Blueprint('origin', url_prefix='/origin')
+#设置模型状态
+model_stats_chatglm = False
+model_loaded_chatglm = False
+
 DEVICE = "cuda"
 DEVICE_ID = "0"
 CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
 
-
+#显存回收
 def torch_gc():
     if torch.cuda.is_available():
         with torch.cuda.device(CUDA_DEVICE):
             torch.cuda.empty_cache()
             torch.cuda.ipc_collect()
-model_stats = False
-model_loaded = False
+
 #使用多线程加载模型
 def load_model():
-    global tokenizer, model, model_loaded
+    global tokenizer, model, model_loaded_chatglm 
     try:
-        model_loaded = True
+        model_loaded_chatglm = True
         tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
         model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
         #完成后将模型状态设置为True
@@ -31,24 +34,40 @@ def load_model():
         print(e)
         model_loaded = False
         return
-    global model_stats
-    model_stats = True
+    global model_stats_chatglm
+    model_stats_chatglm = True
+    return
+#当30分钟后没有访问，将模型状态设置为False，并回收模型
+def reset_model_stats():
+    global model_stats_chatglm,model_loaded_chatglm, model, tokenizer
+    
+    #查看时间
+    time_now = datetime.datetime.now()
+    #如果当前时间与上次访问时间相差30分钟，回收模型
+    if (time_now - time_visit).seconds > 1800:
+        del model
+        del tokenizer
+        model_stats_chatglm = False
+        model_loaded_chatglm = False
     return
 
+#设置定时器，每5秒执行一次
+timer = threading.Timer(5, reset_model_stats)
 @origin_bp.route('/', methods=['POST'])
 async def index(request):
+    global time_visit
     data = request.json
     prompt = data.get('prompt')
     history = data.get('history')
     #获取标准时间
-    time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_visit = datetime.datetime.now()
     #如果模型未加载完成，返回错误信息
-    if not model_stats:
-        if model_loaded:
-            return json({'response': '模型尚未准备就绪','history':[],"time":time})
+    if not model_stats_chatglm:
+        if model_loaded_chatglm:
+            return json({'response': '模型尚未准备就绪','history':[],"status": 500,"time":time_visit.strftime('%Y-%m-%d %H:%M:%S')})
         #使用多线程的方式加载模型
         threading.Thread(target=load_model).start()
-        return json({'response': '模型尚未准备就绪','history':[],"time":time})
+        return json({'response': '模型尚未准备就绪','history':[],"status": 500,"time":time_visit.strftime('%Y-%m-%d %H:%M:%S')})
     #如果模型加载完成，返回结果
     response, history = model.chat(tokenizer,
                                    prompt,
@@ -60,7 +79,8 @@ async def index(request):
         "response": response,
         "history": history,
         "status": 200,
-        "time": time
+        "time": time_visit.strftime('%Y-%m-%d %H:%M:%S')
     }
     torch_gc()
     return json(answer,ensure_ascii=False)
+
