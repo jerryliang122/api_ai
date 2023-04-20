8416e542977b9c52d0bc5075b2688d26df1e9f37
jerryliang122 <jerryliang@jerryliang.top>
2023-04-20 09:56:19 +0000

    重写回收显存代码

--- views/origin.py
+++ views/origin.py
@@ -1,79 +1,80 @@
 from sanic import Blueprint
 from sanic.response import json
-from transformers import AutoTokenizer, AutoModel
-import torch
 import datetime
-import threading
+from transformers import AutoTokenizer, AutoModel 
+import torch
+import asyncio
 
 origin_bp = Blueprint('origin', url_prefix='/origin')
-#设置模型状态
-model_stats_chatglm = False
-model_loaded_chatglm = False
+#全局变量，用于存储模型和 tokenizer
+tokenizer = None
+model = None
 
-DEVICE = "cuda"
-DEVICE_ID = "0"
-CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
+# 全局变量，用于记录上次访问模型的时间
+last_access_time = None
 
-#显存回收
-def torch_gc():
-    if torch.cuda.is_available():
-        with torch.cuda.device(CUDA_DEVICE):
+# 定义一个时间间隔，表示多长时间没有访问模型后自动关闭模型
+TIMEOUT = 1 * 60  # 30 分钟
+
+# 创建一个 asyncio.Event 对象
+stop_event = asyncio.Event()
+
+# 异步任务：加载模型
+async def load_model():
+    global tokenizer
+    global model
+    global last_access_time
+
+    # 加载模型和 tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+    model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+
+    # 记录当前时间
+    last_access_time = datetime.datetime.now()
+
+    # 进入循环，等待下一次访问
+    while not stop_event.is_set():
+        # 计算当前时间和上次访问时间的差值
+        time_since_last_access = (datetime.datetime.now() - last_access_time).total_seconds()
+
+        # 如果超过了指定的时间间隔，则关闭模型
+        if time_since_last_access > TIMEOUT:
+            # 释放 GPU 显存
+            del model
+            # 清空全局变量
+            model = None
+            tokenizer = None
+            last_access_time = None
+            #回收显存
             torch.cuda.empty_cache()
             torch.cuda.ipc_collect()
-model_lock = threading.Lock()
-#使用多线程加载模型
-def load_model():
-    global tokenizer, model, model_loaded_chatglm 
-    model_lock.acquire()
-    try:
-        model_loaded_chatglm = True
-        tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-        model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
-        #完成后将模型状态设置为True
-    except Exception as e:
-        #打印错误日志
-        print(e)
-        model_loaded_chatglm = False
-        return
-    global model_stats_chatglm
-    model_stats_chatglm = True
-    return
-#当30分钟后没有访问，将模型状态设置为False，并回收模型
-def reset_model_stats():
-    global model_stats_chatglm,model_loaded_chatglm, model, tokenizer
-    model_lock.acquire()
-    #查看时间
-    time_now = datetime.datetime.now()
-    #如果当前时间与上次访问时间相差30分钟，回收模型
-    if model_stats_chatglm == False:
-        return
-    if (time_now - time_visit).seconds > 1800:
-        del model
-        del tokenizer
-        torch.cuda.empty_cache()
-        torch.cuda.ipc_collect()
-        model_stats_chatglm = False
-        model_loaded_chatglm = False
-    return
+            break
+
+        # 等待一段时间后再次检查
+        await asyncio.sleep(60)  # 每分钟检查一次
+
 
 @origin_bp.route('/', methods=['POST'])
 async def index(request):
-    global time_visit
+    global tokenizer
+    global model
+    global last_access_time
+
     data = request.json
     prompt = data.get('prompt')
     history = data.get('history')
-    #获取标准时间
-    time_visit = datetime.datetime.now()
-    #如果模型未加载完成，返回错误信息
-    if not model_stats_chatglm:
-        if model_loaded_chatglm:
-            return json({'response': '模型尚未准备就绪','history':[],"status": 500,"time":time_visit.strftime('%Y-%m-%d %H:%M:%S')})
-        #使用多线程的方式加载模型
-        threading.Thread(target=load_model).start()
-        #设置定时器，每5秒执行一次
-        threading.Timer(5, reset_model_stats).start()
-        return json({'response': '模型尚未准备就绪','history':[],"status": 500,"time":time_visit.strftime('%Y-%m-%d %H:%M:%S')})
-    #如果模型加载完成，返回结果
+    # 如果模型尚未加载，则创建异步任务来加载模型
+    if tokenizer is None or model is None:
+        task = asyncio.create_task(load_model())
+        answer = {
+            'response': '模型尚未准备就绪',
+            'history': [],
+            'status': 500,
+            'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
+        return json(answer,ensure_ascii=False)
+    # 记录当前时间
+    last_access_time = datetime.datetime.now()
     response, history = model.chat(tokenizer,
                                    prompt,
                                    history=history,
@@ -84,8 +85,8 @@ async def index(request):
         "response": response,
         "history": history,
         "status": 200,
-        "time": time_visit.strftime('%Y-%m-%d %H:%M:%S')
+        "time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
-    torch_gc()
+
     return json(answer,ensure_ascii=False)
 
