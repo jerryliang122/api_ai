346585d3c163b98eb9a38f68e3d289dfee09eacf
jerryliang122 <jerryliang@jerryliang.top>
2023-08-06 23:05:21 +0800

    更新支持流式传输

--- main.py
+++ main.py
@@ -1,78 +1,34 @@
 import time
 import uvicorn
-from pydantic import BaseModel, Field
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from typing import Any, Dict, List, Literal, Optional, Union
 from sse_starlette.sse import ServerSentEvent, EventSourceResponse
 import datetime
 import asyncio
 from model.chatglm import chatGLM2_6B
+from config import (
+    ModelCard,
+    ChatMessage,
+    ModelList,
+    ChatCompletionRequest,
+    ChatCompletionResponseChoice,
+    DeltaMessage,
+    ChatCompletionResponseStreamChoice,
+    ChatCompletionResponse,
+)
 
 TIMEOUT = 300
 model_lists = ["chatglm2-6b", "chatglm2-6b-lora"]
-
-
-class ModelCard(BaseModel):
-    id: str
-    object: str = "model"
-    created: int = Field(default_factory=lambda: int(time.time()))
-    owned_by: str = "owner"
-    root: Optional[str] = None
-    parent: Optional[str] = None
-    permission: Optional[list] = None
-
-
-class ChatMessage(BaseModel):
-    role: Literal["user", "assistant", "system"]
-    content: str
-
-
-class ModelList(BaseModel):
-    object: str = "list"
-    data: List[ModelCard] = []
-
-
-class ChatCompletionRequest(BaseModel):
-    model: str
-    messages: List[ChatMessage]
-    temperature: Optional[float] = None
-    top_p: Optional[float] = None
-    max_length: Optional[int] = None
-    stream: Optional[bool] = False
-
-
-class ChatCompletionResponseChoice(BaseModel):
-    index: int
-    message: ChatMessage
-    finish_reason: Literal["stop", "length"]
-
-
-class DeltaMessage(BaseModel):
-    role: Optional[Literal["user", "assistant", "system"]] = None
-    content: Optional[str] = None
-
-
-class ChatCompletionResponseStreamChoice(BaseModel):
-    index: int
-    delta: DeltaMessage
-    finish_reason: Optional[Literal["stop", "length"]]
-
-
-class ChatCompletionResponse(BaseModel):
-    model: str
-    object: Literal["chat.completion", "chat.completion.chunk"]
-    choices: List[Union[ChatCompletionResponseChoice, ChatCompletionResponseStreamChoice]]
-    created: Optional[int] = Field(default_factory=lambda: int(time.time()))
-
+loading = None
+last_access_time = None
 
 # 创建一个 asyncio.Event 对象
 stop_event = asyncio.Event()
 
 
 # 模型加载器
-async def timer(model_name):
+async def load_model(model_name):
     global model, loading, time_since_last_access, lora
     if model_name == model_lists[0]:
         model = chatGLM2_6B()
@@ -85,6 +41,8 @@ async def timer(model_name):
 
     # 进入循环，等待下一次访问
     while not stop_event.is_set():
+        if last_access_time == None:
+            break
         # 计算当前时间和上次访问时间的差值
         time_since_last_access = (datetime.datetime.now() - last_access_time).total_seconds()
         # 如果超过了指定的时间间隔，则关闭模型
@@ -125,20 +83,26 @@ async def create_chat_completion(request: ChatCompletionRequest):
     if request.messages[-1].role != "user":
         raise HTTPException(status_code=400, detail="Invalid request")
     if loading == None:
-        pass
+        # 阻塞模式下加载模型
+        await asyncio.create_task(load_model(request.model))
     elif request.model != loading:
         raise HTTPException(status_code=400, detail=f"Model not loaded,Waiting for release: {time_since_last_access}")
     query = request.messages[-1].content
 
     prev_messages = request.messages[:-1]
     if len(prev_messages) > 0 and prev_messages[0].role == "system":
-        query = prev_messages.pop(0).content + query
+        query1 = prev_messages.pop(0).content + query
 
     history = []
     if len(prev_messages) % 2 == 0:
         for i in range(0, len(prev_messages), 2):
             if prev_messages[i].role == "user" and prev_messages[i + 1].role == "assistant":
                 history.append([prev_messages[i].content, prev_messages[i + 1].content])
+    # 流式传输
+    if request.stream:
+        generate = model.stream_chat(query, history, lora=lora, model_id=request.model)
+        last_access_time = datetime.datetime.now()
+        return EventSourceResponse(generate, media_type="text/event-stream")
 
     response, _ = model.chat(query, history=history, lora=lora)
     choice_data = ChatCompletionResponseChoice(
@@ -148,3 +112,5 @@ async def create_chat_completion(request: ChatCompletionRequest):
     return ChatCompletionResponse(model=request.model, choices=[choice_data], object="chat.completion")
 
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
--- model/chatglm.py
+++ model/chatglm.py
@@ -1,6 +1,17 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 import sys
+from config import (
+    ModelCard,
+    ChatMessage,
+    ModelList,
+    ChatCompletionRequest,
+    ChatCompletionResponseChoice,
+    DeltaMessage,
+    ChatCompletionResponseStreamChoice,
+    ChatCompletionResponse,
+)
+import time
 
 sys.path.append("src")
 from peft import PeftModel
@@ -13,16 +24,16 @@ CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
 class chatGLM2_6B:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained(
-            "/root/model/chatglm-6b", trust_remote_code=True, local_files_only=True
+            "/root/model/chatglm2-6b-32k", trust_remote_code=True, local_files_only=True
         )
         model = (
-            AutoModel.from_pretrained("/root/model/chatglm2-6b", trust_remote_code=True, local_files_only=True)
+            AutoModel.from_pretrained("/root/model/chatglm2-6b-32k", trust_remote_code=True, local_files_only=True)
             .half()
             .cuda()
         )
-        self.model = PeftModel.from_pretrained(model, "./chatglm-lora/")
+        self.model = PeftModel.from_pretrained(model, "./chatglm2-lora/")
 
-    def __call__(self, prompt, history, lora):
+    def chat(self, prompt, history, lora):
         if lora:
             response, history = self.model.chat(
                 self.tokenizer, prompt, history=history, max_length=32000, top_p=0.7, temperature=0.95
@@ -38,6 +49,46 @@ class chatGLM2_6B:
                 torch.cuda.empty_cache()
                 torch.cuda.ipc_collect()
         return response, history
+
+    async def stream_chat(self, prompt, history, lora, model_id):
+        choice_data = ChatCompletionResponseStreamChoice(
+            index=0, delta=DeltaMessage(role="assistant"), finish_reason=None
+        )
+        chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
+        yield "{}".format(chunk.json(exclude_unset=True, ensure_ascii=False))
+
+        current_length = 0
+        if lora:
+            for new_response, _ in self.model.stream_chat(self.tokenizer, prompt, history):
+                if len(new_response) == current_length:
+                    continue
+
+                new_text = new_response[current_length:]
+                current_length = len(new_response)
+
+                choice_data = ChatCompletionResponseStreamChoice(
+                    index=0, delta=DeltaMessage(content=new_text), finish_reason=None
+                )
+                chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
+                yield "{}".format(chunk.json(exclude_unset=True, ensure_ascii=False))
+        else:
+            for new_response, _ in self.model.stream_chat(self.tokenizer, prompt, history):
+                if len(new_response) == current_length:
+                    continue
+
+                new_text = new_response[current_length:]
+                current_length = len(new_response)
+
+                choice_data = ChatCompletionResponseStreamChoice(
+                    index=0, delta=DeltaMessage(content=new_text), finish_reason=None
+                )
+                chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
+                yield "{}".format(chunk.json(exclude_unset=True, ensure_ascii=False))
+
+        choice_data = ChatCompletionResponseStreamChoice(index=0, delta=DeltaMessage(), finish_reason="stop")
+        chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
+        yield "{}".format(chunk.json(exclude_unset=True, ensure_ascii=False))
+        yield "[DONE]"
 
     def __del__(self):
         del self.model
