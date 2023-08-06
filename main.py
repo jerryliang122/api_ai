43d1f90dec1bbbb7f514863a9fb4f6e45b1e9166
jerryliang122 <jerryliang@jerryliang.top>
2023-08-06 22:10:34 +0800

    更新api方式支持openai

--- main.py
+++ main.py
@@ -1,14 +1,152 @@
-from sanic import Sanic
-from views.chatglm_p import chatglm_p
-from views.chatglm_l import chatglm_l
-from views.sd import stable_diffusion_bp
-from views.moss import moss_bp
+import time
+import uvicorn
+from pydantic import BaseModel, Field
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from typing import Any, Dict, List, Literal, Optional, Union
+from sse_starlette.sse import ServerSentEvent, EventSourceResponse
+import datetime
+import asyncio
+from model.chatglm import chatGLM2_6B
 
-app = Sanic(__name__)
-app.blueprint(chatglm_p)
-app.blueprint(chatglm_l)
-app.blueprint(stable_diffusion_bp)
-app.blueprint(moss_bp)
+TIMEOUT = 300
+model_lists = ["chatglm2-6b", "chatglm2-6b-lora"]
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+
+class ModelCard(BaseModel):
+    id: str
+    object: str = "model"
+    created: int = Field(default_factory=lambda: int(time.time()))
+    owned_by: str = "owner"
+    root: Optional[str] = None
+    parent: Optional[str] = None
+    permission: Optional[list] = None
+
+
+class ChatMessage(BaseModel):
+    role: Literal["user", "assistant", "system"]
+    content: str
+
+
+class ModelList(BaseModel):
+    object: str = "list"
+    data: List[ModelCard] = []
+
+
+class ChatCompletionRequest(BaseModel):
+    model: str
+    messages: List[ChatMessage]
+    temperature: Optional[float] = None
+    top_p: Optional[float] = None
+    max_length: Optional[int] = None
+    stream: Optional[bool] = False
+
+
+class ChatCompletionResponseChoice(BaseModel):
+    index: int
+    message: ChatMessage
+    finish_reason: Literal["stop", "length"]
+
+
+class DeltaMessage(BaseModel):
+    role: Optional[Literal["user", "assistant", "system"]] = None
+    content: Optional[str] = None
+
+
+class ChatCompletionResponseStreamChoice(BaseModel):
+    index: int
+    delta: DeltaMessage
+    finish_reason: Optional[Literal["stop", "length"]]
+
+
+class ChatCompletionResponse(BaseModel):
+    model: str
+    object: Literal["chat.completion", "chat.completion.chunk"]
+    choices: List[Union[ChatCompletionResponseChoice, ChatCompletionResponseStreamChoice]]
+    created: Optional[int] = Field(default_factory=lambda: int(time.time()))
+
+
+# 创建一个 asyncio.Event 对象
+stop_event = asyncio.Event()
+
+
+# 模型加载器
+async def timer(model_name):
+    global model, loading, time_since_last_access, lora
+    if model_name == model_lists[0]:
+        model = chatGLM2_6B()
+        loading = model_name
+        lora = False
+    elif model_name == model_lists[1]:
+        model = chatGLM2_6B()
+        loading = model_name
+        lora = True
+
+    # 进入循环，等待下一次访问
+    while not stop_event.is_set():
+        # 计算当前时间和上次访问时间的差值
+        time_since_last_access = (datetime.datetime.now() - last_access_time).total_seconds()
+        # 如果超过了指定的时间间隔，则关闭模型
+        if time_since_last_access > TIMEOUT:
+            # 释放 GPU 显存
+            del model
+            # 清空全局变量
+            model = None
+            loading = False
+            break
+        # 每隔一段时间检查一次
+        await asyncio.sleep(5)
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# 设计V1版本的接口
+@app.get("/v1/models", response_model=ModelList)
+async def list_models():
+    global model_args
+    for model in model_lists:
+        model_card = ModelCard(id=model)
+    return ModelList(data=[model_card])
+
+
+@app.post("/v1/chat/completions", response_model=ChatCompletionResponse)
+async def create_chat_completion(request: ChatCompletionRequest):
+    global model, last_access_time
+
+    if request.messages[-1].role != "user":
+        raise HTTPException(status_code=400, detail="Invalid request")
+    if loading == None:
+        pass
+    elif request.model != loading:
+        raise HTTPException(status_code=400, detail=f"Model not loaded,Waiting for release: {time_since_last_access}")
+    query = request.messages[-1].content
+
+    prev_messages = request.messages[:-1]
+    if len(prev_messages) > 0 and prev_messages[0].role == "system":
+        query = prev_messages.pop(0).content + query
+
+    history = []
+    if len(prev_messages) % 2 == 0:
+        for i in range(0, len(prev_messages), 2):
+            if prev_messages[i].role == "user" and prev_messages[i + 1].role == "assistant":
+                history.append([prev_messages[i].content, prev_messages[i + 1].content])
+
+    response, _ = model.chat(query, history=history, lora=lora)
+    choice_data = ChatCompletionResponseChoice(
+        index=0, message=ChatMessage(role="assistant", content=response), finish_reason="stop"
+    )
+    last_access_time = datetime.datetime.now()
+    return ChatCompletionResponse(model=request.model, choices=[choice_data], object="chat.completion")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
--- model/chatglm.py
+++ model/chatglm.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 import sys
+
 sys.path.append("src")
 from peft import PeftModel
 
@@ -8,49 +9,36 @@ DEVICE = "cuda"
 DEVICE_ID = "0"
 CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
 
-class chatGLM_6B():
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("/root/model/chatglm-6b", trust_remote_code=True, local_files_only=True)
-        self.model = AutoModel.from_pretrained("/root/model/chatglm-6b", trust_remote_code=True, local_files_only=True).half().cuda()
-        
 
-    def __call__(self, prompt,history):
-        response, history = self.model.chat(self.tokenizer,
-                                   prompt,
-                                   history=history,
-                                   max_length=2048,
-                                   top_p=0.7,
-                                   temperature=0.95)
-        #回收显存
+class chatGLM2_6B:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "/root/model/chatglm-6b", trust_remote_code=True, local_files_only=True
+        )
+        model = (
+            AutoModel.from_pretrained("/root/model/chatglm2-6b", trust_remote_code=True, local_files_only=True)
+            .half()
+            .cuda()
+        )
+        self.model = PeftModel.from_pretrained(model, "./chatglm-lora/")
+
+    def __call__(self, prompt, history, lora):
+        if lora:
+            response, history = self.model.chat(
+                self.tokenizer, prompt, history=history, max_length=32000, top_p=0.7, temperature=0.95
+            )
+        else:
+            with self.model.disable_adapter():
+                response, history = self.model.chat(
+                    self.tokenizer, prompt, history=history, max_length=32000, top_p=0.7, temperature=0.95
+                )
+        # 回收显存
         if torch.cuda.is_available():
             with torch.cuda.device(CUDA_DEVICE):
                 torch.cuda.empty_cache()
                 torch.cuda.ipc_collect()
         return response, history
-    def __del__(self):
-        del self.model
-        self.model = None
-        self.tokenizer = None
-        torch.cuda.empty_cache()
-        torch.cuda.ipc_collect()
 
-class chatglm_lora():
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("/root/model/chatglm-6b", trust_remote_code=True, local_files_only=True)
-        model = AutoModel.from_pretrained("/root/model/chatglm-6b", trust_remote_code=True, local_files_only=True).half().cuda()
-        model = PeftModel.from_pretrained(model, "./chatglm-lora/")
-        self.model = model.half().cuda()
-
-    def __call__(self, prompt,history):
-        response, history = self.model.chat(self.tokenizer,
-                                   prompt,
-                                   history=history)
-        #回收显存
-        if torch.cuda.is_available():
-            with torch.cuda.device(CUDA_DEVICE):
-                torch.cuda.empty_cache()
-                torch.cuda.ipc_collect()
-        return response, history
     def __del__(self):
         del self.model
         self.model = None
