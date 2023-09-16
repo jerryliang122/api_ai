230ea305ad945e15ac77fb1e1d3c411a364aea53
jerryliang122 <jerryliang@jerryliang.top>
2023-09-16 12:57:01 +0800

    更新model update

--- dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.10.9-slim
-WORKDIR /app
-COPY . /app
-RUN apt-get update  && apt install git -y && apt-get clean
-RUN pip install -r requirements.txt
-CMD python main.py
--- main.py
+++ main.py
@@ -1,92 +1,18 @@
-import time
-import uvicorn
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
-from sse_starlette.sse import ServerSentEvent, EventSourceResponse
-import datetime
-import asyncio
-from model.chatglm import chatGLM2_6B
-from config import (
-    ModelCard,
-    ChatMessage,
-    ModelList,
-    ChatCompletionRequest,
-    ChatCompletionResponseChoice,
-    DeltaMessage,
-    ChatCompletionResponseStreamChoice,
-    ChatCompletionResponse,
-    ChatRequest,
-    ChatResponse,
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+import sys
+import os
+
+# 获取qcloud_secret_id环境变量
+secret_id = os.environ.get("secret_id")
+secret_key = os.environ.get("secret_key")
+region = os.environ.get("region")
+token = None  # 使用临时密钥需要传入 Token，默认为空，可不填
+scheme = "https"  # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
+config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
+client = CosS3Client(config)
+
+response = client.upload_file(
+    Bucket="jerryliang-10052152", LocalFilePath="chatglm2-6b-32K.zip", Key="chatglm2-6b-32K.zip"
 )
-import torch
-import gc
-
-TIMEOUT = 120
-model_lists = ["chatglm2-6b"]
-loading = None
-last_access_time = None
-
-# 创建一个 asyncio.Event 对象
-stop_event = asyncio.Event()
-model = chatGLM2_6B()
-
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-# 设计V1版本的接口
-@app.get("/v1/models", response_model=ModelList)
-async def list_models():
-    global model_args
-    for model in model_lists:
-        model_card = ModelCard(id=model)
-    return ModelList(data=[model_card])
-
-
-@app.post("/v1/chat/completions", response_model=ChatCompletionResponse)
-async def create_chat_completion(request: ChatCompletionRequest):
-    global model, last_access_time
-    query = request.messages[-1].content
-    last_access_time = datetime.datetime.now()
-    prev_messages = request.messages[:-1]
-    if len(prev_messages) > 0 and prev_messages[0].role == "system":
-        query1 = prev_messages.pop(0).content + query
-    history = []
-    if len(prev_messages) % 2 == 0:
-        for i in range(0, len(prev_messages), 2):
-            if prev_messages[i].role == "user" and prev_messages[i + 1].role == "assistant":
-                history.append([prev_messages[i].content, prev_messages[i + 1].content])
-    # 流式传输
-    if request.stream:
-        generate = model.stream_chat(query, history, model_id=request.model, temperature=request.temperature)
-        last_access_time = datetime.datetime.now()
-        return EventSourceResponse(generate, media_type="text/event-stream")
-
-    response, _ = model.chat(query, history=history, temperature=request.temperature)
-    choice_data = ChatCompletionResponseChoice(
-        index=0, message=ChatMessage(role="assistant", content=response), finish_reason="stop"
-    )
-    return ChatCompletionResponse(model=request.model, choices=[choice_data], object="chat.completion")
-
-
-@app.post("/chatglm", response_model=ChatResponse)
-async def create_chat_completion(request: ChatRequest):
-    global model, last_access_time
-    last_access_time = datetime.datetime.now()
-    query = request.prompt
-    history = request.history
-    response, history = model.chat(query, history=history, temperature=request.temperature)
-    return ChatResponse(response=response, status=200, history=history)
-
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000, workers=1)
+print(response["ETag"])
