6440c61f3f1ac6c85e78225fdc8d4fbc2d1f1371
jerryliang122 <jerryliang@jerryliang.top>
2023-09-23 00:08:44 +0800

    更新download

--- download_model.py
+++ download_model.py
@@ -29,48 +29,6 @@ def file_list_url():
 async def download_file(url, filename):
     async with httpx.AsyncClient() as client:
         r = await client.get(url)
-        with open(filename, "wb") as f:
-            f.write(r.content)
-    return True
-
-
-async def main():
-    # 获取list
-    os.mkdir("/tmp/chatglm2-6b-32k")
-    urls = await file_list_url()
-    print(urls)
-
-
-if __name__ == "__main__":
-    from qcloud_cos import CosConfig
-from qcloud_cos import CosS3Client
-import os
-import httpx
-
-# 初始化COS
-secret_id = os.environ.get("secret_id")
-secret_key = os.environ.get("secret_key")
-region = os.environ.get("region")
-token = None
-scheme = "https"
-config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
-client = CosS3Client(config)
-
-
-# file_list
-def file_list_url():
-    urls = []
-    response = client.list_objects(Bucket="ai-1251947439", Prefix="chatglm2-6b-32k/")
-    for file in response["Contents"]:
-        url = client.get_presigned_url(Method="GET", Bucket="ai-1251947439", Key=file["Key"], Expired=120)
-        urls.append((file["Key"], url))
-    return urls
-
-
-# 使用httpx下载
-async def download_file(url, filename):
-    async with httpx.AsyncClient() as client:
-        r = await client.get(url)
         async with aiofiles.open(filename, "wb") as f:
             # 使用await f.write来异步写入文件
             await f.write(r.content)
@@ -78,6 +36,7 @@ async def download_file(url, filename):
 
 async def main():
     # 获取list
+    os.mkdir("/tmp/chatglm2-6b-32k")
     urls = file_list_url()
     # 使用asyncio进行异步下载
     download_tasks = []
